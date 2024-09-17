41cfb28807c0fa95cab368a975f19922ccb7bfdd
Andriana-Petrenko <zlata0707petrenko@gmail.com>
2024-09-17 17:26:15 +0300

    fix styles, add animation

---
+++ src/js/advantages.js
@@ -0,0 +1,15 @@
+if (window.innerWidth < 1440) {
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      } else {
+        entry.target.classList.remove('show');
+      }
+    });
+  });
+
+  document.querySelectorAll('.fade-up').forEach(item => {
+    observer.observe(item);
+  });
+}
--- src/main.js
+++ src/main.js
@@ -1,2 +1,3 @@
 import './js/header.js';
 import './js/how_to_play.js';
+import './js/advantages.js';
