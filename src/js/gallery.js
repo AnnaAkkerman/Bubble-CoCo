f3493ae61bfb5643476c560658748492757016fe
Andriana-Petrenko <zlata0707petrenko@gmail.com>
2024-09-18 20:20:49 +0300

    add gallery section

---
+++ src/js/gallery.js
@@ -0,0 +1,32 @@
+import Swiper from 'swiper/bundle';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css/bundle';
+document.addEventListener('DOMContentLoaded', () => {
+  const swiperContainer = document.querySelector('.swip-container');
+  if (swiperContainer) {
+    const swiper = new Swiper('.swip-container', {
+      modules: [Navigation, Pagination],
+      loop: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      slidesPerView: 1,
+      spaceBetween: 20,
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+        },
+        1440: {
+          slidesPerView: 3,
+        },
+      },
+    });
+  } else {
+    console.error('Swiper container not found');
+  }
+});
--- src/main.js
+++ src/main.js
@@ -2,3 +2,4 @@ import './js/header.js';
 import './js/reviews.js';
 import './js/how_to_play.js';
 import './js/advantages.js';
+import './js/gallery.js';
